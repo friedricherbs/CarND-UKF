29b857adc5b849de45f0669cf05ed5b0964787c9
Erbs <uidg5371@contiwan.com>
2017-04-08 13:24:48 +0200

    parameter update

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -41,10 +41,10 @@ UKF::UKF() {
         0,      0,     0,     0,      100000; 
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 0.35; 
+  std_a_ = 0.5; 
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.35; 
+  std_yawdd_ = 0.5; 
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15; 
